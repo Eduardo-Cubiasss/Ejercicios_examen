af226c1bc3a1f7a8402995b342c00db72f898910
kevinPoncema <131317454+kevinPoncema@users.noreply.github.com>
2025-05-13 09:45:55 +0000

    si

--- sales_analysis.h
+++ sales_analysis.h
@@ -14,11 +14,13 @@ public:
 //constructor con valores predeterminados para pDepartmentName (string vacio) y pEmployeeCount (0)
     TSalesDepartment(std::string pDepartmentName = "", int pEmployeeCount = 0);
     ~TSalesDepartment(); 
-
+    //añade ventas
     void AddEmployeeSales(double pSale);
+    //calcula el promedio de ventas
     double CalculateAverageSales() const;
     //metodo que compara si el promedio supero cierta cifra (50000)
     bool MeetsPerformanceThreshold() const;
+    //devuelve el indice del mejor empleados (venta mas alta)
     int FindTopPerformingEmployeeIndex() const;
 
     std::string GetDepartmentName() const;
